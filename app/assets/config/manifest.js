d7dcdb53d5e3bfa7a3689b7163b233c53f189cc3
Miguel Augusto <miguelmasche@gmail.com>
2024-06-02 15:30:58 -0700

    add about us section

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,9 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_directory ../javascripts .js
+const randomfn = function(arr){
+  for (let element of arr) {
+    console.log(element)
+  }
+
+} 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-
+  get 'about', to: 'about#index'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
-    post   :add_item
-    post   :remove_item
+    post :add_item
+    post :remove_item
   end
 
   resources :orders, only: [:create, :show]
@@ -24,51 +24,51 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  # get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  # get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  # resources :products
 
   # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
+  # resources :products do
+  #   member do
+  #     get 'short'
   #     post 'toggle'
   #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+  #
+  #   collection do
+  #     get 'sold'
+  #   end
+  # end
+
+  # Example resource route with sub-resources:
+  # resources :products do
+  #   resources :comments, :sales
+  #   resource :seller
+  # end
+
+  # Example resource route with more complex sub-resources:
+  # resources :products do
+  #   resources :comments
+  #   resources :sales do
+  #     get 'recent', on: :collection
+  #   end
+  # end
+
+  # Example resource route with concerns:
+  # concern :toggleable do
+  #   post 'toggle'
+  # end
+  # resources :posts, concerns: :toggleable
+  # resources :photos, concerns: :toggleable
 
   # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # namespace :admin do
+  #   # Directs /admin/products/* to Admin::ProductsController
+  #   # (app/controllers/admin/products_controller.rb)
+  #   resources :products
+  # end
 end
